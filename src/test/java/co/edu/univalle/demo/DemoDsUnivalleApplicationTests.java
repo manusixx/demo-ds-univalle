76f318eed1706a81483a6929e33b6850a0856f68
ManuelPastrana <alexander15950@gmail.com>
2026-06-09 08:59:50 -0500

    refactor: DemoDSUNivaleApplicationTests.java extends now from BaseIntegrationTest to use test profile in the integration tests

--- src/test/java/co/edu/univalle/demo/DemoDsUnivalleApplicationTests.java
+++ src/test/java/co/edu/univalle/demo/DemoDsUnivalleApplicationTests.java
@@ -2,10 +2,13 @@ package co.edu.univalle.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import co.edu.univalle.demo.integration.BaseIntegrationTest;
 
+/** Test de carga del contexto de Spring Boot. */
 @SpringBootTest
-class DemoDsUnivalleApplicationTests {
+class DemoDsUnivalleApplicationTests extends BaseIntegrationTest  {
 
+    /** Verifica que el contexto de Spring arranca correctamente. */
     @Test
     void contextLoads() {
     }
